f57170903d424428240ec76798d3d043d423e6a9
Ms. E <RElinich@gmail.com>
2018-08-08 10:44:07 -0400

    Updated with functional Demonstration

--- Examples/public_html/pyramidJS/script.js
+++ Examples/public_html/pyramidJS/script.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 
 //External JS document demonstrating the Pyramid Exercise. Remember to link to this document:
-for (var line="#";	line.length<8;	line += "#"){
+/*for (var line="#";	line.length<8;	line += "#"){
 	document.getElementById("pyramid").innerHTML=line;
+}*/
+
+var theText="";
+var line="#";
+for(var i=0; i<8; i++){
+	line.length<8;
+	line+="#";
+	theText+= line + "<br/>";
 }
+
+document.getElementById("pyramid").innerHTML=theText;
