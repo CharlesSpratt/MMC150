1ccafcce001b4e8b72621439c293320c7fe9c807
Ms. E <RElinich@gmail.com>
2018-08-08 10:13:31 -0400

    Basic For laid out

--- Examples/public_html/pyramidJS/script.js
+++ Examples/public_html/pyramidJS/script.js
@@ -1,1 +1,6 @@
 // JavaScript Document
+
+//External JS document demonstrating the Pyramid Exercise. Remember to link to this document:
+for (var line="#";	line.length<8;	line += "#"){
+	document.getElementById("pyramid").innerHTML=line;
+}
