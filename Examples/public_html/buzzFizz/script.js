40cc6b0479caa96c7caece3366e26bb15a5f73af
Ms. E <RElinich@gmail.com>
2018-08-08 12:16:46 -0400

    Updated main index with new link & Added BuzzFizz baseline site

---
+++ Examples/public_html/buzzFizz/script.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+//Base Script to work from:
+/*
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
+*/
+
+//New Script for output to document:
+//Some additional Links that were reviewed:
+//https://www.w3schools.com/js/js_loop_for.asp
+//https://www.w3schools.com/js/js_if_else.asp
+var myOutput="";
+
+for(var i=1; i<=100; i++){
+	if(i % 3===0){
+		myOutput+="Fizz" + "<br />";
+	}else if(i % 5===0){
+		myOutput+="Buzz" + "<br />";
+	}
+	
+document.getElementById("buzzFizzOut").innerHTML=myOutput || i;
+}
+
