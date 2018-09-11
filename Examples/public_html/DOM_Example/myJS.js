6ed45bf77f7fb017672346a940682e323ccdab26
RElinich <RElinich@gmail.com>
2018-09-11 06:57:41 -0400

    9/11

---
+++ Examples/public_html/DOM_Example/myJS.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+
+//alert("JS Doc hit.");
+document.getElementById("example2").innerHTML="In fact, I might have something written but because of the DOM of the document, you have multiple options on how to display information";
